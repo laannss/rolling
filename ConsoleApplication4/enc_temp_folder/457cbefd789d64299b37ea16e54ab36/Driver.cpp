0e84bb8fc97407893a364343e1c397a69e1d4a6d
Sandro <Sandro@DESKTOP-GRU28CN>
2017-02-24 20:40:42 +0400

    change is good

---
+++ ConsoleApplication4/enc_temp_folder/457cbefd789d64299b37ea16e54ab36/Driver.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+#include "Sphere.h"
+int main() {
+
+	Sphere unitSphere;
+	Sphere mySphere(5.1);
+	cout << unitSphere << endl;
+
+	mySphere.setRadius(-64.5);
+	cout << mySphere.getRadius() << endl;
+
+	mySphere.setDiameter(-65.5);
+	cout << mySphere.getDiameter() << endl;
+
+	mySphere.setCircumference(-66.5);
+	cout << mySphere.getCircumference() << endl;
+
+	mySphere.setArea(-76.5);
+	cout << mySphere.getArea() << endl;
+
+	mySphere.setVolume(-621);
+	cout << mySphere.getVolume() << endl;
+
+	getchar();
+}
---
+++ ConsoleApplication4/enc_temp_folder/f1546b45df8c575954bebc4ed9260dc/Sphere.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+using namespace std;
+#include "Sphere.h"
+#define PI 3.14
+
+/* Default and explicit initiliziation*/
+Sphere::Sphere()
+	: Radius(1)
+{
+}
+Sphere::Sphere(double initRadius)
+{
+	if (initRadius > 0) {
+		Radius = initRadius;
+	}
+	else {
+		cerr << "*** Invalid input for Sphere object, created with default value of 1 ***\n";
+		Radius = 1;
+	}
+}
+/*Accesors*/
+double Sphere::getRadius() const {
+	return Radius;
+}
+
+double Sphere::getDiameter() const{
+	return (Radius * 2);
+}
+
+double Sphere::getCircumference() const{
+	return (2 * PI * Radius);
+}
+double Sphere::getArea() const{
+	return (4 * PI * Radius * Radius);
+}
+double Sphere::getVolume() const{
+	return (PI * Radius * Radius * Radius * 4.0 / 3.0);
+}
+/* Mutators */
+void Sphere::setRadius(double newRadius) {
+	if (newRadius > 0) {
+		Radius = newRadius;
+	}
+	else {
+		cerr << "*** Invalid input for Sphere object ***\n";
+	}
+}
+void Sphere::setDiameter(double newDiameter) {
+	if (newDiameter > 0){
+		Radius = newDiameter / 2.0;
+	}
+	else {
+		cerr << "*** Invalid input for Sphere object ***\n";
+	}
+
+}
+void Sphere::setCircumference(double newCircumference) {
+	if (newCircumference > 0) {
+		Radius = newCircumference / (2.0*PI);
+	}
+	else {
+		cerr << "*** Invalid input for Sphere object ***\n";
+	}
+
+
+}
+void Sphere::setArea(double newArea) {
+	if (newArea > 0) {
+		Radius = sqrt(newArea / (4.0*PI));
+	}
+	else {
+		cerr << "*** Invalid input for Sphere object ***\n";
+	}
+}
+void Sphere::setVolume(double newVolume) {
+	if (newVolume > 0) {
+		Radius = cbrt(newVolume*3.0 / (4.0*PI));
+	}
+	else {
+		cerr << "*** Invalid input for Sphere object ***\n";
+	}
+}
+/* I/O */
+void Sphere::display(ostream & out) const
+{
+	out << Radius;
+}
+
+void Sphere::write(istream & in)
+{
+	double rad;
+	in >> rad;
+
+	if (rad > 0) {
+		Radius = rad;
+	}
+	else {
+		cerr << "*** Invalid input for Sphere object ***\n";
+	}
+}
+
+/* >> and << */
+
+ostream & operator<<(ostream & out, const Sphere & s) {
+	s.display(out);
+	return out;
+}
+
+istream & operator >> (istream & in, Sphere & s) {
+	s.write(in);
+	return in;
+}
+
+/***** Relational operators *****/
+bool operator<(const Sphere & s1, const Sphere & s2) {
+	return s1.getRadius() < s2.getRadius();
+}
+bool operator>(const Sphere & s1, const Sphere & s2) {
+	return s1.getRadius() > s2.getRadius();
+}
+bool operator==(const Sphere & s1, const Sphere & s2) {
+	return s1.getRadius() == s2.getRadius();
+}
+bool operator<=(const Sphere & s1, const Sphere & s2) {
+	return s1.getRadius() <= s2.getRadius();
+}
+bool operator>=(const Sphere & s1, const Sphere & s2) {
+	return s1.getRadius() >= s2.getRadius();
+}
+bool operator!=(const Sphere & s1, const Sphere & s2) {
+	return s1.getRadius() != s2.getRadius();
+}
