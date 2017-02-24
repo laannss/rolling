393760fb17fa4b36d8cc592ba1be8ddff04b7ffe
laannss <laannss@gmail.com>
2017-02-24 16:25:20 +0400

    yay

---
+++ ConsoleApplication4/ConsoleApplication4/Driver.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+#include "Sphere.h"
+
+int main() {
+	Sphere test;
+	Sphere test2 = 2;
+}
--- ConsoleApplication4/ConsoleApplication4/Sphere.h
+++ ConsoleApplication4/ConsoleApplication4/Sphere.h
@@ -1,8 +1,27 @@
-#pragma once
+#include <iostream>
+using namespace std;
+
 class Sphere
 {
 public:
+	/* Default Constructor Radius = 1.0 */
 	Sphere();
-	~Sphere();
-};
+	/* Explicit value constructor */
+	Sphere(double initRadius);
 
+	/* Accessors */
+	double getRadius();
+	double getDiameter();
+	double getCircumference();
+	double getArea();
+	double getVolume();
+
+	/* Mutators */
+	void setRadius();
+
+	/* I/O */
+	void display(ostream & out) const;
+
+private:
+	double Radius;
+};
