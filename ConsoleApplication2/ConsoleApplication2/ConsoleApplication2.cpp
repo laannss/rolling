3858a013e9819ea8848459f09191a02d982199da
Sandro <Sandro@DESKTOP-GRU28CN>
2017-02-24 15:20:16 +0400

    Change

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,26 @@
+// ConsoleApplication2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	/*rame
+	rame
+	rame
+	rame
+	rame
+	rame
+	rame
+	rame
+	rame
+	rame
+	rame
+	rame
+	rame
+	rame
+	*/
+    return 0;
+}
+
