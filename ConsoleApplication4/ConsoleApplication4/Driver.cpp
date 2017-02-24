d4687bad21bd6b77bb20303c751373a165d185d0
Sandro <Sandro@DESKTOP-GRU28CN>
2017-02-24 17:22:49 +0400

    Changes!

--- ConsoleApplication4/ConsoleApplication4/Driver.cpp
+++ ConsoleApplication4/ConsoleApplication4/Driver.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 using namespace std;
 #include "Sphere.h"
-
 int main() {
 	Sphere test;
 	Sphere test2 = 2;
-	cout << test2;
+	cout << test2<<endl	;
+	
+	cout << test2.getRadius() << endl;
+	cout << test2.getCircumference() << endl;
+	cout << test2.getDiameter() << endl;
+	cout << test2.getArea() << endl;
+	cout << test2.getVolume() << endl;
+
 	getchar();
 }
